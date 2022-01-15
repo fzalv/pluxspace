f08f0ace984dbdc3a878a8da34f3b21e776029c6
Fauzi Alvan <youhaveamessag3@gmail.com>
2022-01-15 12:53:29 +0700

    detail page

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -10,4 +10,8 @@ class FrontendController extends Controller
     public function index(Request $request){
         return view('pages.frontend.index');
     }
+
+    public function details(Request $request, $slug){
+        return view('pages.frontend.details');
+    }
 }
