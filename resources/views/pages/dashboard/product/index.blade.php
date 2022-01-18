ce54f7948c2314dbfb38068914a9ff996242e2e1
Fauzi Alvan <youhaveamessag3@gmail.com>
2022-01-18 21:19:14 +0700

    CRUD Produk Create

--- resources/views/pages/dashboard/product/index.blade.php
+++ resources/views/pages/dashboard/product/index.blade.php
@@ -39,7 +39,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="mb-10">
-                <a href="{{route('dashboard.product.index') }}" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded shadow-lg">
+                <a href="{{route('dashboard.product.create') }}" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded shadow-lg">
                     + Create Product
                 </a>
             </div>
