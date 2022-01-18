b3df081024d69c84ebc49d5c6d54eb68569aaba8
Fauzi Alvan <youhaveamessag3@gmail.com>
2022-01-18 15:39:22 +0700

    install ulang tailwindcss, crud produk - index

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\FrontendController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,6 +25,6 @@ Route::middleware(['auth:sanctum', 'verified'])->name('dashboard.')->prefix('das
     Route::get('/', [DashboardController::class, 'index'])->name('index');
 
     Route::middleware(['admin'])->group(function () {
-        //
+        Route::resource('product', ProductController::class);
     });
 });
