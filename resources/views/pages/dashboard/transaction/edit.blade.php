8a68809212d20a3a1980313dfef4bff13b40c526
Fauzi Alvan <youhaveamessag3@gmail.com>
2022-02-06 11:53:09 +0700

    CRUD transaksi edit

--- resources/views/pages/dashboard/transaction/edit.blade.php
+++ resources/views/pages/dashboard/transaction/edit.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Product &raquo; {{$item->name}} &raquo; Edit
+            Transaction &raquo; {{$item->name}} &raquo; Edit
         </h2>
     </x-slot>
 
@@ -24,25 +24,27 @@
                     </div>
                 </div>
                 @endif
-                <form action="{{route('dashboard.product.update', $item->id)}}" class="w-full" method="POST" enctype="multipart/form-data">
+                <form action="{{route('dashboard.transaction.update', $item->id)}}" class="w-full" method="POST" enctype="multipart/form-data">
                     @csrf
                     @method('PUT')
                     <div class="flex flex-wrap -mx-3 mb-6 space-y-6">
                         <div class="w-full px-3">
                             <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Nama</label>
-                            <input type="text" value="{{ old('name') ?? $item->name }}" name="name" placeholder="Nama Produk" class="block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                            <select name="status" class="block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                                <option value="{{$item->status}}">{{$item->status}}</option>
+                                <option disabled>--------------</option>
+                                <option value="PENDING">PENDING</option>
+                                <option value="SUCCESS">SUCCESS</option>
+                                <option value="CHALLENGE">CHALLENGE</option>
+                                <option value="FAILED">FAILED</option>
+                                <option value="SHIPPING">SHIPPING</option>
+                                <option value="SHIPPED">SHIPPED</option>
+                            </select>
                         </div>
-                        <div class="w-full px-3">
-                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Description</label>
-                            <textarea name="description" class="block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">{!! old('description') ?? $item->description !!}</textarea>
-                        </div>
-                        <div class="w-full px-3">
-                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Harga</label>
-                            <input type="number" value="{{ old('price') ?? $item->price }}" name="price" placeholder="Harga Produk" class="block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
-                        </div>
+
                         <div class="w-full px-3">
                             <button type="submit" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded shadow-lg">
-                                Update Produk
+                                Update Transaction
                             </button>
                         </div>
                     </div>
@@ -50,8 +52,4 @@
             </div>
         </div>
     </div>
-    <script src="https://cdn.ckeditor.com/4.16.0/standard/ckeditor.js"></script>
-    <script>
-        CKEDITOR.replace('description');
-    </script>
 </x-app-layout>
