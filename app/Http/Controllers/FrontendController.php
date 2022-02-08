0358701ddfcb3aff0373fd9fb774d8cee3d87840
Fauzi Alvan <youhaveamessag3@gmail.com>
2022-02-08 11:35:54 +0700

    detail produk - rekomendasi produk

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -11,13 +11,16 @@ class FrontendController extends Controller
     public function index(Request $request)
     {
         $products = Product::with(['galleries'])->latest()->get();
+
+
         return view('pages.frontend.index', compact('products'));
     }
 
     public function details(Request $request, $slug)
     {
         $product = Product::with(['galleries'])->where('slug', $slug)->firstOrFail();
-        return view('pages.frontend.details', compact('product'));
+        $recommendations = Product::with(['galleries'])->inRandomOrder()->limit(4)->get();
+        return view('pages.frontend.details', compact('product', 'recommendations'));
     }
 
     public function cart(Request $request)
